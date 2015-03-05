eaef12b13a6daef0e26d3726f90ed9a57cbed0a5
Chris Crook <ccrook@gmail.com>
2015-03-05 17:47:33 -0500

    Typo on app name, regenerated

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Facebook::Application.config.secret_token = 'acc1ab651aa7303f9d171c97b2c18e74a399e4e559e4bdb37cce2f9e9e062689017e3f35d6f03e79784bcb0ddc641671693cc0db0c7c3e9fd4c5896c6fc9f893'
