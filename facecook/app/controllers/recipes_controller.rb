1c40916330eda3a384d60259d041ed9cb535b7f5
Chris Blackburn <burnicus8@gmail.com>
2015-03-06 19:04:21 -0500

    remove old version folder and content

--- facecook/app/controllers/recipes_controller.rb
+++
@@ -1,7 +0,0 @@
-class RecipesController < ApplicationController
-  def index
-    @keyword = params[:keyword] || 'chocolate'
-    result = Recipe.for @keyword
-    @results = result['results']
-  end
-end
